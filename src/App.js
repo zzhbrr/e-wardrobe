cdc7aca85087953bcfa28c34683d142154ed2c18
chigusa111 <8972522+chigusa111@user.noreply.gitee.com>
2022-11-22 16:46:54 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './main/main_page';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
