38a5a5a017899d4a8851d0c1eccc357150d41a18
xxayt <3494085277@qq.com>
2022-11-26 18:00:45 +0800

    Outfits页面创建

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import UserCenter from './components/UserCenter/index';
 import Login from './components/login/login';
 import Register from './components/register/register';
+import Outfits from './components/outfits/outfits';
 import io from 'socket.io-client'
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-  Navigate
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useNavigate,
+    Navigate
 } from "react-router-dom";
 import React from 'react';
 
@@ -17,48 +18,52 @@ const socket = io('ws://localhost:8000')
 // var userName = "", isLogin = false;
 
 function App() {
-  localStorage.clear();
-  const [isLogin, setIsLogIn] = React.useState(false);
-  const [userName, setUserName] = React.useState(localStorage.getItem('userName'));
+    localStorage.clear();
+    const [isLogin, setIsLogIn] = React.useState(false);
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName'));
 
-  function handleLogin(username) {
-    setUserName(username);
-    // console.log('userName set to ', username);
-    // alert('userName set to ', username);
-    setIsLogIn(true);
-    // userName = username;
-    // isLogin = true;
-  }
+    function handleLogin(username) {
+        setUserName(username);
+        // console.log('userName set to ', username);
+        // alert('userName set to ', username);
+        setIsLogIn(true);
+        // userName = username;
+        // isLogin = true;
+    }
 
   // React.useEffect(()=>{
   //   console.log('!!userName: ', userName)
   //   console.log('!!isLogin: ', isLogin);
   // }, )
 
-      
-  return (
-    <Routes>
-      <Route exact path="/login" element={
-        userName === null ? 
-        <div className="App">
-          <Login socket={socket} handleLogin={handleLogin}/>
-        </div> : <Navigate to="/usercenter" />
-        } /> 
-      <Route exact path="/register" element={
-        <div className="App">
-          <Register socket={socket}/>
-        </div>
-        } />
-      <Route exact path="/usercenter" element={
-        <div className="App">
-          <UserCenter socket={socket} isLogin={isLogin} userName={userName}/>
-        </div>
-      }/>
-      <Route exact path="/" element={<Navigate to="/usercenter"></Navigate>} />
-      {/* <Router exact path="/wardorbe" element={}/> */}
-      {/* <Router exact path="/diary" element={}/> */}
-    </Routes>
-  );
+    return (
+        <Routes>
+        <Route exact path="/login" element={
+            userName === null ? 
+            <div className="App">
+                <Login socket={socket} handleLogin={handleLogin}/>
+            </div> : <Navigate to="/usercenter" />
+        }/> 
+        <Route exact path="/register" element={
+            <div className="App">
+                <Register socket={socket}/>
+            </div>
+        }/>
+        <Route exact path="/usercenter" element={
+            <div className="App">
+                <UserCenter socket={socket} isLogin={isLogin} userName={userName}/>
+            </div>
+        }/>
+        <Route exact path="/outfits" element={
+            <div className="App">
+                <Outfits socket={socket} userName={userName}/>
+            </div>
+        }/>
+        <Route exact path="/" element={<Navigate to="/usercenter"></Navigate>} />
+        {/* <Router exact path="/wardorbe" element={}/> */}
+        {/* <Router exact path="/diary" element={}/> */}
+        </Routes>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/outfits/outfits.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./outfits.css"
+import cloth_pic from "./cloth.png"
+
+export default function Outfits(props){
+    const default_product={
+        pid:    0,
+        src:    cloth_pic
+    }
+    const default_outfit={
+        oid:        0,
+        name:       "default",
+        upwear:     default_product,
+        downwear:   default_product,
+        coat:       default_product,
+        shoe:       default_product,
+        decoration: default_product
+    }
+
+    return(
+        <div>
+            <div className="header">
+                <h1>搭配详情</h1>
+            </div>
+
+            {/* <div className="product_card"> */}
+                <button className="coat_pos">
+                    <div className="outfit_content">外套</div>
+                    <img src={default_outfit.coat.src} className="outfit_img"/>
+                </button>
+                <button className="upwear_pos">
+                    <div className="outfit_content">上衣</div>
+                    <img src={default_outfit.upwear.src} className="outfit_img"/>
+                </button>
+                <button className="downwear_pos">
+                    <div className="outfit_content">下装</div>
+                    <img src={default_outfit.downwear.src} className="outfit_img"/>
+                </button>
+                <button className="shoe_pos">
+                    <div className="outfit_content">鞋子</div>
+                    <img src={default_outfit.shoe.src} className="outfit_img"/>
+                </button>
+                <button className="decoration_pos">
+                    <div className="outfit_content">饰品</div>
+                    <img src={default_outfit.decoration.src} className="outfit_img"/>
+                </button>
+            {/* </div> */}
+        </div>
+        /* <div className="outfits">
+            <h1>全部穿搭</h1>
+            <div className="list">
+                {outfit_list.map((outfit)=>
+                    <div className="outfit_card" key={outfit.oid}>
+                        <div className="product_card">
+                            <div className="outfit_content">上衣</div>
+                            <img src={outfit.upwear.src} className="outfit_img"/>
+                        </div>
+                        <div className="product_card">
+                            <div className="outfit_content">下装</div>
+                            <img src={outfit.downwear.src} className="outfit_img"/>
+                        </div>
+                        <div className="product_card">
+                            <div className="outfit_content">外套</div>
+                            <img src={outfit.coat.src} className="outfit_img"/>
+                        </div>
+                        <div className="product_card">
+                            <div className="outfit_content">鞋子</div>
+                            <img src={outfit.shoe.src} className="outfit_img"/>
+                        </div>
+                        <div className="product_card">
+                            <div className="outfit_content">饰品</div>
+                            <img src={outfit.decoration.src} className="outfit_img"/>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </div> */
+    )
+}
