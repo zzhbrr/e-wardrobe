6e234b1785f84a615dede39da006c822c01dd6bd
chigusa111 <8972522+chigusa111@user.noreply.gitee.com>
2022-12-02 14:05:42 +0800

    修改了route，增加向组详情页跳转

--- frontend/src/components/GroupDetail/GroupInfo/index.js
+++ frontend/src/components/GroupDetail/GroupInfo/index.js
@@ -4,25 +4,22 @@ import "./index.css"
 
 export default function GroupInfo(props){
     const group_info=props.group_info;
-    const reqGroupInfo=props.reqGroupInfo;
-    const reqGroupInfoChange=props.handleChangeInfo;
+    // const reqGroupInfo=props.reqGroupInfo;
+    // const reqGroupInfoChange=props.handleChangeInfo;
     // if(group_info==null){
     //     reqGroupInfo()
     // }
-    React.useEffect(()=>{
-        if(group_info.uid===-1&&!props.init_state){
-            reqGroupInfo(group_info.groupname)
-
-        }
-    })
+    // React.useEffect(()=>{
+    //     if(group_info.uid===-1&&!props.init_state){
+    //         reqGroupInfo(group_info.gid)
+    //     }
+    // })
 
     return(
         <div className="group_info">
             <h1 className=" group left">{group_info.groupname} </h1>
-            <div className="gid left">gid：{group_info.uid}</div>
-            <div className="group_content left">性别：{group_info.gender}</div>
-            <div className="group_content left">年龄：{group_info.age}</div>
-            <div className="group_content left">职业：{group_info.profession}</div>
+            <div className="gid left">gid：{group_info.gid}</div>
+            <div className="group_content left">简介：{group_info.intro}</div>
             {/* <button className="change_btn">修改信息</button> */}
             {/* <ChangeInfo className="change_btn left" handleChangeInfo={reqGroupInfoChange}/> */}
         </div>
