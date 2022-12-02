b8f6ada39c39849ed1158591ec1c853620277680
xxayt <3494085277@qq.com>
2022-12-02 18:08:13 +0800

    aa

--- frontend/src/components/History/History.js
+++ frontend/src/components/History/History.js
@@ -1,17 +1,22 @@
-// cloth
+// History
 import React from "react";
 import "./History.css";
 
 export default function HistoryDetail(props){
-    const username = props.username;
+    // const username = props.username;
     const [history_detail, set_history_detail] = React.useState({
         date: "2022-12-01",
         climate: "晴天",
         situation: "约会",
-        top_id: "0"
+        top_id: "0",
+        bottom_id: "0",
+        coat_id: "0",
+        shoe_id: "0",
+        ornament_id: "0"
     })
     React.useEffect(() => {
         props.socket.on('getAllHistorySuccess', (res) => {
+            console.log("sdff");
             set_history_detail(res.histories);
             console.log(res);
         })
@@ -22,8 +27,8 @@ export default function HistoryDetail(props){
     return(
         <div>
             <div className="History_head">
-                <h1>{"zz"}的历史记录</h1>
-                <div className="History_around">
+                <h1 className="History_title">{"zz"}的历史记录</h1>
+                <div className="History_around">ddd
                 </div>
                 <img src={history_detail.img_src} className="cloth_img"/>
             </div>
